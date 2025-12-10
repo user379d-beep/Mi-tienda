a7cb58c2ba0b280eedd754e10072f9baf9b7c00e
user379d-beep <jhonydiaz673@gmail.com>
2025-12-09 20:22:01 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-document.getElementById("pedidoForm").addEventListener("submit", function(e) {
-    e.preventDefault();
+document.getElementById("btnEnviar").addEventListener("click", async () => {
+  
+  const data = {
+    cliente: document.getElementById("cliente").value,
+    producto: document.getElementById("producto").value,
+    cantidad: Number(document.getElementById("cantidad").value),
+    precio: Number(document.getElementById("precio").value),
+    total: Number(document.getElementById("cantidad").value) * Number(document.getElementById("precio").value)
+  };
 
-    const estado = document.getElementById("estado");
-    estado.textContent = "Enviando pedido...";
+  const url = "https://script.google.com/macros/s/AKfycbx0fz6_F6iir4_-h5C3BVhHZCSGt5FPVDQOHarCOCKPiYvEQPE0DpA86rAKdW8O5aU/exec";
 
-    const formData = new FormData(this);
+  const respuesta = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "text/plain" }
+  });
 
-    fetch("https://script.google.com/macros/s/AKfycbx0fz6_F6iir4_-h5C3BVhHZCSGt5FPVDQOHarCOCKPiYvEQPE0DpA86rAKdW8O5aU/exec", {
-        method: "POST",
-        body: formData
-    })
-    .then(r => r.text())
-    .then(resp => {
-        estado.textContent = "Pedido enviado correctamente ✔";
-        document.getElementById("pedidoForm").reset();
-    })
-    .catch(err => {
-        estado.textContent = "Error enviando pedido ❌";
-    });
-
+  const json = await respuesta.json();
+  alert("Pedido enviado ✔️");
 });
