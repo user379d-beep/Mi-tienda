f4326b0c0641861397ff438741ac16c3906479a8
user379d-beep <jhonydiaz673@gmail.com>
2025-12-09 20:27:04 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.getElementById("btnEnviar").addEventListener("click", async () => {
-  
+  const estado = document.getElementById("estado");
+  estado.textContent = "Enviando pedido...";
+
   const data = {
     cliente: document.getElementById("cliente").value,
     producto: document.getElementById("producto").value,
@@ -10,12 +12,19 @@ document.getElementById("btnEnviar").addEventListener("click", async () => {
 
   const url = "https://script.google.com/macros/s/AKfycbx0fz6_F6iir4_-h5C3BVhHZCSGt5FPVDQOHarCOCKPiYvEQPE0DpA86rAKdW8O5aU/exec";
 
-  const respuesta = await fetch(url, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "text/plain" }
-  });
+  try {
+    const respuesta = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "text/plain" }
+    });
 
-  const json = await respuesta.json();
-  alert("Pedido enviado ✔️");
+    const json = await respuesta.json();
+
+    estado.textContent = "Pedido enviado correctamente ✔";
+    document.getElementById("pedidoForm").reset();
+  } 
+  catch (err) {
+    estado.textContent = "Error enviando pedido ❌";
+  }
 });
